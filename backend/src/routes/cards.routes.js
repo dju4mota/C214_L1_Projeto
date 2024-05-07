afe1b52ef2acd59aa06a3d85f74ac7506ec5ddd6
dju4mota <fernandomotajunior2@gmail.com>
2024-05-06 22:35:34 -0300

    Correção da resposta de status

--- backend/src/routes/cards.routes.js
+++ backend/src/routes/cards.routes.js
@@ -4,7 +4,7 @@ exports.criarCard = function (req, res, next) {
     const novoCard = req.body
 
     if (novoCard.nome == undefined || novoCard.usuario == undefined) {
-        return res.json({ error: "Informe o nome do Card e a qual usuário ele pertence!" }).send(400)
+        return res.status(400).json({ error: "Informe o nome do Card e a qual usuário ele pertence!" })
     }
 
     novoCard.estado = novoCard.estado == undefined || novoCard.estado == "" ? novoCard.estado = "To Do" : novoCard.estado
@@ -13,6 +13,6 @@ exports.criarCard = function (req, res, next) {
         return res.send(201);
     }).catch((err) => {
         console.log("Ocorreu um erro na criação do card", err)
-        return res.json({ error: "Erro interno de servidor" }).send(500)
+        return res.status(500).json({ error: "Erro interno de servidor" })
     });
 }
--- backend/src/routes/usuario.routes.js
+++ backend/src/routes/usuario.routes.js
@@ -5,13 +5,13 @@ exports.criarUsuario = function (req, res, next) {
     req.body.listas = []
 
     if (nome == undefined || login == undefined || senha == undefined) {
-        return res.json({ error: "Informe todos os dados necessários!" }).send(400)
+        return res.status(400).json({ error: "Informe todos os dados necessários!" })
     }
 
     Usuario.create(req.body).then(function () {
         return res.send(201);
     }).catch((err) => {
         console.log("Ocorreu um erro na criação de usuário", err)
-        return res.json({ error: "Erro interno de servidor" }).send(500)
+        return res.status(500).json({ error: "Erro interno de servidor" })
     });
 }
