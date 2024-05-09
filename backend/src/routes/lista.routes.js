ee26830275302632fa356f48346d4799227ec428
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2024-05-09 09:05:20 -0300

    Melhorias nas rotas de lista do usuário

--- backend/src/routes/lista.routes.js
+++ backend/src/routes/lista.routes.js
@@ -4,12 +4,15 @@ exports.criarLista = async function (req, res, next) {
     const novaLista = req.body
     let usuario, usuarioAtualizado
 
-    if (novaLista.nome == undefined || novaLista.idUsuario == undefined) {
+    if (!novaLista.nome || !novaLista.idUsuario) {
         return res.status(400).json({ error: "Informe o nome da lista e a qual usuário ela pertence!" })
     }
 
     try {
         usuario = await Usuario.findById(novaLista.idUsuario)
+        if (!usuario) {
+            return res.status(404).json({ error: "Usuário não encontrado" });
+        }
     } catch (err) {
         console.log("Ocorreu um erro na busca do Usuário", err)
         return res.status(500).json({ error: "Erro interno de servidor" })
@@ -24,12 +27,12 @@ exports.criarLista = async function (req, res, next) {
         }, {
             new: true
         })
+        return res.status(201).json(usuarioAtualizado.listas)
     } catch (err) {
         console.log("Ocorreu um erro no update do Usuário", err)
         return res.status(500).json({ error: "Erro interno de servidor" })
     }
 
-    return res.json(usuarioAtualizado.listas)
 }
 
 exports.buscarListas = async function (req, res, nxt) {
@@ -40,6 +43,9 @@ exports.buscarListas = async function (req, res, nxt) {
 
     try {
         const usuario = await Usuario.findById(req.body.idUsuario)
+        if (!usuario) {
+            return res.status(404).json({ error: "Usuário não encontrado" });
+        }
         return res.json(usuario.listas)
     } catch (error) {
         console.log('Erro ao buscar as listas do usuário')
@@ -55,6 +61,9 @@ exports.atualizarLista = async function(req, res, next) {
 
     try {
         const usuario = await Usuario.findById(req.body.idUsuario)
+        if (!usuario) {
+            return res.status(404).json({ error: "Usuário não encontrado" });
+        }
         const listasAtualizadas = usuario.listas.map(listaAtual => listaAtual.nome === req.body.nomeLista ? req.body.listaAtualizada : listaAtual);
         const usuarioAtualizado = await Usuario.findByIdAndUpdate(req.body.idUsuario, {
             listas: listasAtualizadas
@@ -77,6 +86,9 @@ exports.excluirLista = async function (req, res, nxt) {
 
     try {
         const usuario = await Usuario.findById(req.body.idUsuario)
+        if (!usuario) {
+            return res.status(404).json({ error: "Usuário não encontrado" });
+        }
         const listasAtualizadas = usuario.listas.filter(listaAtual => listaAtual.nome != req.body.nomeLista);
         const usuarioAtualizado = await Usuario.findByIdAndUpdate(req.body.idUsuario, {
             listas: listasAtualizadas
